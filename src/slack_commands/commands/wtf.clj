6fae4bda4e2737e136e11e052504d0550deb1548
PieterJanVdb <pj.vandenbussche@gmail.com>
2021-03-18 02:56:15 +0100

    fix blocks formatting

--- src/slack_commands/commands/wtf.clj
+++ src/slack_commands/commands/wtf.clj
@@ -30,10 +30,8 @@
   (let [graphics (.createGraphics image)
         font (Font. "Arial" Font/PLAIN 35)
         x-offset (get-x-offset name)]
-    (println "drawing on image...")
     (.setFont graphics font)
     (.drawString graphics name x-offset 381)
-    (println "done drawing on image!")
     image))
 
 (defn image->base64 [image]
@@ -42,7 +40,6 @@
       (.encodeToString (Base64/getEncoder) (.toByteArray baos))))
 
 (defn generate-image [name]
-  (println "generating image...")
   (-> (get-template)
       ImageIO/read
       (draw-name name)))
@@ -54,16 +51,15 @@
       upload))
 
 (defn format-wtf [name link]
-  {:type "image"
-   :alt_text (str name " what the fuck are you talking about")
-   :image_url link})
+  {:blocks [{:type "image"
+             :alt_text (str name " what the fuck are you talking about")
+             :image_url link}]})
 
 (defn handle-wtf [name]
   (try
     (if (valid-name? name)
       (let [link (get-image-link name)
             msg (format-wtf name link)]
-        (println "msg" msg)
         {:success true :msg msg})
       (throw (ex-info VAL_ERROR {:cause :bad-input})))
     (catch clojure.lang.ExceptionInfo ex
