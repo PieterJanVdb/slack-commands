02deb70b8990caf0a9996ff500eb891190683aa1
PieterJanVdb <pj.vandenbussche@gmail.com>
2024-07-06 15:21:00 +0200

    trigger redeploy

--- src/slack_commands/error.clj
+++ src/slack_commands/error.clj
@@ -1,4 +1,4 @@
 (ns slack-commands.error)
 
 (defn get-error-message [e]
-  (if (:cause (ex-data e)) (.getMessage e) "Something went wrong..."))
+  (if (:cause (ex-data e)) (.getMessage e) "Something went wrong"))
